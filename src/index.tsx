69a816371f509bcc71af0c4e1f20ad472e4dbd83
RaeAdvent <aeraeneldis@yahoo.com>
2020-09-01 00:23:52 +0700

    First commit before refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,42 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import axios from 'axios'
 
-interface Props {
-  text: string
+type logging = Boolean
+
+/**
+ * @param logging <Boolean> - Enable logging for each request, default: false
+ */
+
+const useAxios: React.FC<logging> = (logging: logging = false) => {
+  const initAxios = axios
+
+  const source = initAxios.CancelToken.source()
+
+  initAxios.interceptors.request.use(
+    function (config: any) {
+      config.cancelToken = source.token
+      return config
+    },
+    function (err: any) {
+      if (initAxios.isCancel(err)) {
+        const cancelReport = {
+          title: 'request aborted',
+          err: err,
+        }
+        logging && console.table(cancelReport)
+        return Promise.reject(cancelReport)
+      } else {
+        logging && console.error(err)
+        return Promise.reject(err)
+      }
+    }
+  )
+
+  React.useEffect(() => {
+    source.cancel()
+  })
+
+  return initAxios
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export default useAxios
